e2b1139d8c2f2a720f3a45aec6de6276619a1b3b
viewfromoffice <viewfromoffice@gmail.com>
2018-11-29 17:31:04 +0800

    texture test
    with problem, texture is not displays in place

--- Project1/hello_triangle_shining-multi.cpp
+++ Project1/hello_triangle_shining-multi.cpp
@@ -153,8 +153,6 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw our first triangle
-		glUseProgram(shaderProgram);
 
 		// change color
 		float timeValue = glfwGetTime();
